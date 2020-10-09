23e265ac2f98fbc915000102fb4dda1fb32bfb3d
unknown <fddxzf@gmail.com>
2020-10-09 13:15:42 +0530

    calculate the part time employee wages

--- employeewage.sh
+++ employeewage.sh
@@ -10,16 +10,21 @@
    else
        echo "Employee is Abscent";
    fi
-   #calculate daily employeewage UC
-   isPresent=1;
-   randomCheck=$(( RANDOM%2 ));
-   if [[ $isPresent -eq $randomCheck ]];
-	 then
-           empRatePerHr=20;
-           empHrs=8;
-           salary=$(($empHrs*$empRatePerHr));
- 	 else
-       salary=0;
-    fi
+   # Add pattime employee wage
+   isPartTime=1;
+   isFullTime=2;
+   empRatePerHr=20;
+   randomCheck=$(( RANDOM%3 ));
+ 	
+   if [[ $isFullTime -eq $randomCheck ]];
+   then
+ 	empHrs=8;
+   elif [[ $isPartTime -eq $randomCheck ]];
+   then
+	empHrs=4;
+   else
+	empHrs=0;
+   fi
+        salary=$(($empHrs*$empRatePerHr));
 
 
