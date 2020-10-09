f07dd1ea661095d4aa369f84aa74e949eb51f445
unknown <fddxzf@gmail.com>
2020-10-09 13:57:29 +0530

    Refactor code using functions

--- employeewage.sh
+++ employeewage.sh
@@ -10,32 +10,38 @@
    else
        echo "Employee is Abscent";
    fi
-   #calulate employee wages till the total working days is reached or month is reached
-   # CONSTANTS FOR THE PROGRAM
-   IS_PART_TIME=1;
-   IS_FULL_TIME=2;
-   MAX_HRS_IN_MONTH=10;
-   EMP_RATE_PER_HR=20;
-   NUM_WORKING_DAYS=20;
-  
-   # VARIABLES
-   totalEmpHrs=0;
-   totalWorkingDays=0;
 
-   while (( $(( $totalEmpHrs < $MAX_HRS_IN_MONTH )) && $(( $totalWorkingDays < $NUM_WORKING_DAYS )) ))
-   do
-	((totalWorkingDays++))
- 	empCheck=$((RANDOM%3))
-	case $empCheck in
-		$IS_FULL_TIME)
-    		 empHrs=8;;
-		$IS_PART_TIME)
-        	 empHrs=4;;
- 		*)
-		 empHrs=0;;
-	esac
-  	totalEmpHrs=$(($totalEmpHrs+$empHrs))
-    done
-	totalEmpHrs=$(($totalEmpHrs*$EMP_RATE_PER_HR));
+   # using function to calculate employee wage
+   # CONSTANTS FOR THE PROGRAM
+        IS_PART_TIME=1;
+        IS_FULL_TIME=2;
+        MAX_HRS_IN_MONTH=10;
+        EMP_RATE_PER_HR=20;
+        NUM_WORKING_DAYS=20;
+
+        # VARIABLES
+        totalEmpHrs=0;
+        totalWorkingDays=0;
+
+        function getWorkingHours( ) {
+     case $1 in
+         $IS_FULL_TIME)
+            workHrs=8;;
+         $IS_PART_TIME)
+            workHrs=4;;
+         *)
+            workHrs=0;;
+      esac
+                echo "$workHrs"
+     }
+
+     while (( $(( $totalEmpHrs < $MAX_HRS_IN_MONTH )) && $(( $totalWorkingDays < $NUM_WORKING_DAYS )) ))
+     do
+       ((totalWorkingDays++))
+       getWorkingHours $((RANDOM%3))
+       totalEmpHrs=$(($totalEmpHrs+$workHrs))
+   done
+       totalEmpHrs=$(($totalEmpHrs*$EMP_RATE_PER_HR));
+
 
 
