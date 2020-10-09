47a12e9034ec646d3303fd567218aef31b808e7d
unknown <fddxzf@gmail.com>
2020-10-09 13:49:01 +0530

    calculate total salary for a month

--- employeewage.sh
+++ employeewage.sh
@@ -10,26 +10,32 @@
    else
        echo "Employee is Abscent";
    fi
-   # calculate Wages for a month
-   isPartTime=1;
-   isFullTime=2;
-   totalSalary=0;
-   empRatePerHr=20;
-   numWorkingDays=20;
-   
-   for (( day=1; day<=$numWorkingDays; day++ ))
-   do
+   #calulate employee wages till the total working days is reached or month is reached
+   # CONSTANTS FOR THE PROGRAM
+   IS_PART_TIME=1;
+   IS_FULL_TIME=2;
+   MAX_HRS_IN_MONTH=10;
+   EMP_RATE_PER_HR=20;
+   NUM_WORKING_DAYS=20;
+  
+   # VARIABLES
+   totalEmpHrs=0;
+   totalWorkingDays=0;
 
-      empCheck=$(( RANDOM%3 ));
-         case $empCheck in
-                $isFullTime)
-                        empHrs=8;;
-                $isPartTime)
-                        empHrs=4;;
-                *)
-                        empHrs=0;;
-         esac
-         salary=$(($empHrs*$empRatePerHr));
+   while (( $(( $totalEmpHrs < $MAX_HRS_IN_MONTH )) && $(( $totalWorkingDays < $NUM_WORKING_DAYS )) ))
+   do
+	((totalWorkingDays++))
+ 	empCheck=$((RANDOM%3))
+	case $empCheck in
+		$IS_FULL_TIME)
+    		 empHrs=8;;
+		$IS_PART_TIME)
+        	 empHrs=4;;
+ 		*)
+		 empHrs=0;;
+	esac
+  	totalEmpHrs=$(($totalEmpHrs+$empHrs))
     done
+	totalEmpHrs=$(($totalEmpHrs*$EMP_RATE_PER_HR));
 
 
