1d3a872f428bc20714649366370e592ee695bbdd
unknown <fddxzf@gmail.com>
2020-10-09 13:41:12 +0530

    calculate  salary along month

--- employeewage.sh
+++ employeewage.sh
@@ -10,20 +10,26 @@
    else
        echo "Employee is Abscent";
    fi
-   # add use case statements to  code
+   # calculate Wages for a month
    isPartTime=1;
    isFullTime=2;
+   totalSalary=0;
    empRatePerHr=20;
-   empCheck=$(( RANDOM%3 ));
+   numWorkingDays=20;
+   
+   for (( day=1; day<=$numWorkingDays; day++ ))
+   do
 
-   case $empCheck in 
-	$isFullTime)
-		empHrs=8;;
-	$isPartTime)
-	        empHrs=4;;
-               	*)
-		empHrs=0;;
-   esac
-   salary=$(( $empHrs*$empRatePerHr ));
+      empCheck=$(( RANDOM%3 ));
+         case $empCheck in
+                $isFullTime)
+                        empHrs=8;;
+                $isPartTime)
+                        empHrs=4;;
+                *)
+                        empHrs=0;;
+         esac
+         salary=$(($empHrs*$empRatePerHr));
+    done
 
 
