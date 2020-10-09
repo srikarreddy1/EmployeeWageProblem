d3c70ef960eec04e42795fe51ad060d39128f2ea
unknown <fddxzf@gmail.com>
2020-10-09 14:06:24 +0530

    store the daily wages in array

--- employeewage.sh
+++ employeewage.sh
@@ -11,19 +11,20 @@
        echo "Employee is Abscent";
    fi
 
-   # using function to calculate employee wage
+   
+   #store the daily wage along with the total wage
    # CONSTANTS FOR THE PROGRAM
-        IS_PART_TIME=1;
-        IS_FULL_TIME=2;
-        MAX_HRS_IN_MONTH=10;
-        EMP_RATE_PER_HR=20;
-        NUM_WORKING_DAYS=20;
+   IS_PART_TIME=1;
+   IS_FULL_TIME=2;
+   MAX_HRS_IN_MONTH=10;
+   EMP_RATE_PER_HR=20;
+   NUM_WORKING_DAYS=20;
 
-        # VARIABLES
-        totalEmpHrs=0;
-        totalWorkingDays=0;
+   # VARIABLES
+   totalEmpHrs=0;
+   totalWorkingDays=0;
 
-        function getWorkingHours( ) {
+   function getWorkingHours( ) {
      case $1 in
          $IS_FULL_TIME)
             workHrs=8;;
@@ -31,17 +32,14 @@
             workHrs=4;;
          *)
             workHrs=0;;
-      esac
-                echo "$workHrs"
+     esac
      }
-
+ 
      while (( $(( $totalEmpHrs < $MAX_HRS_IN_MONTH )) && $(( $totalWorkingDays < $NUM_WORKING_DAYS )) ))
      do
-       ((totalWorkingDays++))
-       getWorkingHours $((RANDOM%3))
-       totalEmpHrs=$(($totalEmpHrs+$workHrs))
-   done
-       totalEmpHrs=$(($totalEmpHrs*$EMP_RATE_PER_HR));
-
-
-
+        ((totalWorkingDays++))
+     getWorkingHours $((RANDOM%3))
+         totalEmpHrs=$(($totalEmpHrs+$workHrs))
+         dailyWage[$totalWorkingDays]=$(($workHrs*$EMP_RATE_PER_HR))
+     done
+         totalEmpHrs=$(($totalEmpHrs*$EMP_RATE_PER_HR))
