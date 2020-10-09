b24db8931d6039b67c05f7dc09b26d8c557e1339
unknown <fddxzf@gmail.com>
2020-10-09 13:04:12 +0530

    employee attendence code added

--- employeewage.sh
+++ employeewage.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
  
    echo "welcome to employee wage proble solution"
+   #employee is present or absent uc
+   isPresent=1;
+   randomCheck=$(( RANDOM%2 ));
+   if [[ $isPresent -eq $randomCheck ]]
+   then
+       echo "Employee is Present";
+   else
+       echo "Employee is Abscent";
+   fi
