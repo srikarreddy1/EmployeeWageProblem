2f6f48b4dab62e2f792e6862d76e82bc212913bf
unknown <fddxzf@gmail.com>
2020-10-09 13:32:23 +0530

    solve using switch case

--- employeewage.sh
+++ employeewage.sh
@@ -10,21 +10,20 @@
    else
        echo "Employee is Abscent";
    fi
-   # Add pattime employee wage
+   # add use case statements to  code
    isPartTime=1;
    isFullTime=2;
    empRatePerHr=20;
-   randomCheck=$(( RANDOM%3 ));
- 	
-   if [[ $isFullTime -eq $randomCheck ]];
-   then
- 	empHrs=8;
-   elif [[ $isPartTime -eq $randomCheck ]];
-   then
-	empHrs=4;
-   else
-	empHrs=0;
-   fi
-        salary=$(($empHrs*$empRatePerHr));
+   empCheck=$(( RANDOM%3 ));
+
+   case $empCheck in 
+	$isFullTime)
+		empHrs=8;;
+	$isPartTime)
+	        empHrs=4;;
+               	*)
+		empHrs=0;;
+   esac
+   salary=$(( $empHrs*$empRatePerHr ));
 
 
